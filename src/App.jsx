cc710e4c0716a47844e37799d0a94e7871da68d7
Daniel H <dannyhdz27@gmail.com>
2023-04-20 09:57:25 -0500

    created register form. working on token

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 import fetchData from "./API/api";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import PostsComponent from "./components/posts";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <h2>Strangers Things</h2>
+      <nav>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/posts">Posts</Link>
+        </button>
+      </nav>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/posts" element={<PostsComponent />} />
       </Routes>
     </div>
---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function RegisterForm({setToken}){
+    export default function RegisterForm(){
+        const [username, setUsername] = useState("")
+        const [password, setPassword] = useState("")
+
+        // const { setToken, user } = useAuth();
+        // console.log("User from RegisterForm: ", user); 
+
+        async function handleSubmit(e){
+            e.preventDefault();
+            try {
+                const result = await registerUser(username, password)
+                console.log("Result in Component: ", result);
+                setToken(result.data.token);
+            } catch (error) {
+                console.error(error);
+            }
+
+
+        } 
+
+    }
+
+    return (
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="text"
+              name="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button>Submit</button>
+          </form>
+        </div>
+      );
+}
