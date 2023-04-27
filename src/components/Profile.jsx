cdbc4cebebfc1aa88103046244f49109f7cb2843
Daniel H <dannyhdz27@gmail.com>
2023-04-27 09:51:29 -0500

    welcome message shows on login

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,22 +5,19 @@ import { useEffect, useState } from "react";
 export const Profile = () => {
   const { token } = useAuth();
 
-  const [name, setName] = useState([]);
+  const [name, setName] = useState("");
   useEffect(() => {
     async function displayName() {
-      const result = await fetchMe();
+      const result = await fetchMe(token);
       setName(result.data.username);
     }
     displayName();
   }, []);
-  {
-    name.map((names) => {
-      return (
-        <div>
-          <h2>PROFILE</h2>
-          <h1>Hello {name.data.username}</h1>
-        </div>
-      );
-    });
-  }
+
+  return (
+    <div>
+      <h2>PROFILE</h2>
+      <h1>Hello {name}</h1>
+    </div>
+  );
 };
